378ed7ac02915082f3ea94506392f6c33bf6cdc3
majidshabani <majid.shabani@yandex.com>
2023-06-07 14:56:30 +0330

    v4

--- cat_piece_count.sql
+++ cat_piece_count.sql
@@ -2,12 +2,10 @@ SET VERIFY OFF
 COMP SUM OF PIECE_CNT ON REPORT
 BREAK ON REPORT
 /* Author: Majid Shabani */
-SELECT DECODE(BACKUP_TYPE,'D','Full Backup','I','Incremental Backup'
- ,'L','Backup of Archivelog')BACKUP_TYPE, COUNT(*) PIECE_CNT
-FROM RMAN.RC_BACKUP_SET_DETAILS
+SELECT DECODE(BACKUP_TYPE,'D','Full Backup','I','Incremental Backup' ,'L','Backup of Archivelog')BACKUP_TYPE, COUNT(*) PIECE_CNT
+FROM RC_BACKUP_SET_DETAILS
 WHERE UPPER(DB_NAME)=UPPER('&1')
- AND SESSION_KEY=&2
-GROUP BY DECODE(BACKUP_TYPE,'D','Full Backup','I','Incremental Backup','L','Backup of
-Archivelog');
+  AND SESSION_KEY=&2
+GROUP BY DECODE(BACKUP_TYPE,'D','Full Backup','I','Incremental Backup','L','Backup of Archivelog');
 CLEAR BREAK
 SET VERIFY ON
